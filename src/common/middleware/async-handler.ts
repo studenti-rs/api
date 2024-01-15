8b539fcf8218d6f7404d9d8eb0d44c2ffb2a01d7
Mladen Ruzicic <ruzicic@gmail.com>
2024-01-15 10:18:40 +0100

    Add async handler middleware

---
+++ src/common/middleware/async-handler.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+
+/**
+ * Wraps an asynchronous request handler function with error handling middleware.
+ * * This asyncHandler function is a common pattern in Express.js to avoid having to write try/catch
+ * * blocks in each asynchronous middleware function. Instead, you can wrap your asynchronous
+ * * middleware functions with asyncHandler, and it will automatically catch any errors and pass them
+ * * to the next middleware function.
+ * @param fn - The asynchronous request handler function.
+ * @returns A middleware function that handles asynchronous errors.
+ */
+export function asyncHandler(fn: (req: Request, res: Response) => Promise<any>) {
+  return (req: Request, res: Response, next: any) => {
+    return fn(req, res).catch(next)
+  }
+}
