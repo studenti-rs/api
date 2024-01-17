76b6e406107f9a91116781e10b829c294e3622a5
Mladen Ruzicic <ruzicic@gmail.com>
2024-01-17 10:08:34 +0100

    Fix build errors

--- src/modules/file-processing/file-processing.controller.ts
+++ src/modules/file-processing/file-processing.controller.ts
@@ -2,6 +2,6 @@ import { Request, Response } from 'express'
 
 import { asyncHandler } from '@common/middleware/async-handler'
 
-export const processFile = asyncHandler(async (req: Request, res: Response) => {
+export const processFile = asyncHandler(async (_req: Request, res: Response) => {
 	res.send('processFile')
 })
